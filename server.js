ebae9788cfb5ab32f1e911e047d344dca25ac866
RoneyThomas <roneythomas6@gmail.com>
2022-01-11 15:55:31 -0500

    Add new routes

--- server.js
+++ server.js
@@ -32,13 +32,13 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const pollsRoutes = require("./routes/polls");
+const voteRoutes = require("./routes/vote");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/poll", pollsRoutes(db));
+app.use("/api/vote", voteRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
