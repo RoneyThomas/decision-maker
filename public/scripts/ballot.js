cff1c39ffbc6d9fcffe23246ffbf1fe281867123
TJ <michael.btj@hotmail.com>
2022-01-12 19:54:46 +0000

    fix bug

--- public/scripts/ballot.js
+++ public/scripts/ballot.js
@@ -1,18 +1,22 @@
+$(function () {
+  console.log("sometext");
+  loadBallot();
+});
 
-const escape = function(str) {
+const escape = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
 
-const createBallotElement = function(choice) {
+const createBallotElement = function (input) {
   // Create XSS safe text
-  const choice = `${escape(choice)}`;
+  const choice = `${escape(input)}`;
   return $(`<li class="ballot-entry">${choice}</li>`);
 };
 
-const numToRank = function(num) {
+const numToRank = function (num) {
   switch (true) {
     // odd cases where rank 11, 12, 13 isn't 11st, 12nd, 13rd
     case num === 11:
@@ -29,11 +33,11 @@ const numToRank = function(num) {
     case num % 10 === 3:
       return num + "rd"
     default:
-    return num + "th"
+      return num + "th"
   }
 };
 
-const renderBallot = function(choices) {
+const renderBallot = function (choices) {
   let i = 0;
   for (const choice of choices) {
     i++;
@@ -42,10 +46,12 @@ const renderBallot = function(choices) {
     $('#rank-container').append($rankNumber);
     $('#choice-container').append($choice);
   }
-}
+};
 
-const loadBallot = function() {
-  $.get("/vote/", (data) => {
-    renderBallot(choices);
+const loadBallot = function () {
+  $.get("/api/vote/votes", (data) => {
+    renderBallot(data);
   });
 };
+
+
