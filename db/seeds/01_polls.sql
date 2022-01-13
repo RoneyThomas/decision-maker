279ba8a98d4c69125ba6b8f5ee3abffd825ccea8
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2022-01-12 22:11:48 -0500

    add line to add the pgcrypto EXTENSION to DB, to use the UUID generator function, generate 14 UUID to use in the choices and votes seed file, add generated UUID in the polls seed, start seed for votes table

--- db/seeds/01_polls.sql
+++ db/seeds/01_polls.sql
@@ -1,16 +1,16 @@
 -- Users table seeds here (Example)
-INSERT INTO polls (decision, description, electorate_size, start_time, end_time, email)
-VALUES ('1st Pool', 'preferred colour', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
-        ('2nd Pool', 'preferred car', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
-        ('3rd Pool', 'preferred plane', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
-        ('4tt Pool', 'preferred truck', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
-        ('5th Pool', 'preferred day', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
-        ('6th Pool', 'preferred holiday', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
-        ('7th Pool', 'preferred drink', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
-        ('8th Pool', 'preferred breakfast', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
-        ('9th Pool', 'preferred dish', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
-        ('10th Pool', 'preferred sport', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
-        ('11th Pool', 'preferred Toronto Attraction', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
-        ('12th Pool', 'preferred home type', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
-        ('13th Pool', 'preferred commute vehicle', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
-        ('14th Pool', 'preferred sports team', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com');
+INSERT INTO polls (id,decision, description, electorate_size, start_time, end_time, email)
+VALUES  ('49e5cff4-2431-4194-a109-ec5ed65f99cf','1st Pool', 'preferred colour', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
+        ('c8a50f06-2904-49fe-bc36-4adc105cbd4e','2nd Pool', 'preferred car', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
+        ('a59fbaf5-76cb-4fa0-8d40-69ee675dcac8','3rd Pool', 'preferred plane', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
+        ('5e30cc7c-401a-49b4-80af-4cb6ebed6321','4tt Pool', 'preferred truck', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
+        ('d05ee026-d221-4ffc-964a-f28b88c2cd28','5th Pool', 'preferred day', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
+        ('7e4e4ed4-99c7-4ef0-a08a-9455adb167f0','6th Pool', 'preferred holiday', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
+        ('7e581ef9-fc7b-429f-8218-8df19dcb44be','7th Pool', 'preferred drink', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
+        ('156e6c22-58d8-48ad-9d4d-9bb66d7b1813','8th Pool', 'preferred breakfast', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
+        ('43728b43-3c73-4f3f-baed-e690fab108eb','9th Pool', 'preferred dish', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
+        ('8504212b-d05c-448c-a661-fb2810cbcc13','10th Pool', 'preferred sport', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
+        ('a4a1c906-02cc-427f-b17b-17744ce280c3','11th Pool', 'preferred Toronto Attraction', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
+        ('e6bb8c55-a934-471b-8991-3ea92725bd00','12th Pool', 'preferred home type', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
+        ('331f11a4-4523-4093-8acc-4be06cd9a48e','13th Pool', 'preferred commute vehicle', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com'),
+        ('5f7baec6-e00d-401b-b422-b46f9689675d','14th Pool', 'preferred sports team', 4, now(), now() + interval '4 hours', 'vladv9@gmail.com');
