b80ba796347492f7305701ba0c066bac8ffaa821
RoneyThomas <roneythomas6@gmail.com>
2022-01-14 00:16:21 -0500

    Added tailwind and logo

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,1 @@
-// Client facing scripts here
 
-const addCandidate = () => {
-  var node = document.createElement("li");
-  var textnode = document.createTextNode(document.getElementById("candidate").value);
-  node.appendChild(textnode);
-  document.getElementById("candidates").appendChild(node);
-  document.getElementById("candidate").value = "";
-}
