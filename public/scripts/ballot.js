12bd9c549856958a7b15a97e57f7d3ae3d3ad3e3
TJ <michael.btj@hotmail.com>
2022-01-14 04:27:38 +0000

    comments and cleaning code

--- public/scripts/ballot.js
+++ public/scripts/ballot.js
@@ -1,12 +1,14 @@
-$(function () {
+$(document).ready(function () {
   console.log("Ready");
 
   loadBallot();
+  // Enable Drag and drop
   $( "#choice-container" ).sortable();
 
   $('form').submit((event) => {
     event.preventDefault();
 
+    // Parse poll vote
     const poll_votes = $("#choice-container").sortable("toArray").map(x=>x.slice(7));
     $.post("/api/vote/dummy", { poll_votes });
     console.log(poll_votes);
@@ -14,34 +16,14 @@ $(function () {
   })
 });
 
-
 const createBallotElement = function (input) {
   // Create XSS safe text
   const choice = `${escape(input)}`;
+
+  // Create dynamic ids so the choice placement can be ready
   return $(`<li id="choice_${choice}" class="ballot-entry"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>${choice}</li>`);
 
 };
-
-// const numToRank = function (num) {
-//   switch (true) {
-//     // odd cases where rank 11, 12, 13 isn't 11st, 12nd, 13rd
-//     case num === 11:
-//       return "11th";
-//     case num === 12:
-//       return "12th";
-//     case num === 13:
-//       return "13th";
-//     //regular cases
-//     case num % 10 === 1:
-//       return num + "st";
-//     case num % 10 === 2:
-//       return num + "nd";
-//     case num % 10 === 3:
-//       return num + "rd";
-//     default:
-//       return num + "th";
-//   }
-// };
 
 const renderBallot = function (choices) {
   let i = 0;
